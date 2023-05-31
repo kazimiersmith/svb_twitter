86e7417e328eafcdbb1cddfbe69f4f2b22b8bf49
Kazimier Smith <kazimiersmith@10-16-155-181.dynapool.wireless.nyu.edu>
2023-05-31 11:21:09 -0400

    variable names

--- python/prepare_data.py
+++ python/prepare_data.py
@@ -28,7 +28,14 @@ fig = root / 'fig'
 default_dpi = 300
 
 # For now, just read in the results from searching #svb, since reading all the data would be slow
-posts = pd.read_json(data_twitter / 'hashtag_svb.json', lines = True, encoding = 'utf-16')
+hashtag_svb = pd.read_json(data_twitter / 'hashtag_svb.json', lines = True, encoding = 'utf-16')
+search_silicon_valley_bank = pd.read_json(data_twitter / 'search_silicon_valley_bank.json', lines = True, encoding = 'utf-16')
+
+# Combine the two dataframes
+posts = pd.concat([hashtag_svb, search_silicon_valley_bank], ignore_index = True)
+
+# Drop duplicate ids
+posts = posts.drop_duplicates(subset = 'id')
 
 # Read in Stata files
 bjzz = pd.read_stata(data_in / 'bjzz.dta')
@@ -37,22 +44,25 @@ price_minute = pd.read_stata(data_in / 'price_minute.dta')
 
 # Aggregate data
 resample_method = 'min'
-ctm_seconds_agg_methods = {'price': 'mean'}
-ctm_seconds = ctm.set_index('tc').resample(resample_method).agg(ctm_seconds_agg_methods).reset_index()
-ctm_seconds = ctm_seconds.rename(columns = {'tc': 'time'})
+ctm_agg_methods = {'price': 'mean'}
+ctm_agg = ctm.set_index('tc').resample(resample_method).agg(ctm_agg_methods).reset_index()
+ctm_agg = ctm_agg.rename(columns = {'tc': 'time'})
 
-posts_seconds_agg_methods = {'id': 'count'}
-posts_seconds = posts.set_index('date').resample(resample_method).agg(posts_seconds_agg_methods).reset_index()
-posts_seconds = posts_seconds.rename(columns = {'date': 'time', 'id': 'num_posts'})
+posts_agg_methods = {'id': 'count'}
+posts_agg = posts.set_index('date').resample(resample_method).agg(posts_agg_methods).reset_index()
+posts_agg = posts_agg.rename(columns = {'date': 'time', 'id': 'num_posts'})
 
 # Convert Tweet times to New York time and then remove time zone information (we don't really need it)
-posts_seconds['time'] = posts_seconds['time'].dt.tz_convert('America/New_York')
-posts_seconds['time'] = posts_seconds['time'].dt.tz_localize(None)
+posts_agg['time'] = posts_agg['time'].dt.tz_convert('America/New_York')
+posts_agg['time'] = posts_agg['time'].dt.tz_localize(None)
 
-# Merge posts seconds and ctm_seconds
-posts_ctm_seconds = pd.merge(posts_seconds, ctm_seconds, on = 'time', how = 'outer')
+# Merge posts seconds and ctm_agg
+posts_ctm_agg = pd.merge(posts_agg, ctm_agg, on = 'time', how = 'outer')
 
-posts_ctm_seconds.to_pickle(data_out / 'posts_ctm_seconds.pickle')
+# Cumulative number of posts
+posts_ctm_agg['cum_num_posts'] = posts_ctm_agg['num_posts'].cumsum()
+
+posts_ctm_agg.to_pickle(data_out / 'posts_ctm_agg.pickle')
 
 # Write to CSV for debugging
-posts_ctm_seconds.to_csv(temp / 'posts_ctm_seconds.csv')
+posts_ctm_agg.to_csv(temp / 'posts_ctm_agg.csv')
