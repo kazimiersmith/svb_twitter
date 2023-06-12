18aea878d88330799184611ab12a1af9ad393303
Kazimier Smith <kas1112@stern.nyu.edu>
2023-06-12 15:09:08 -0400

    Search for hashtags with twitter-search rather than twitter-hashtag so I can set an end date

--- scrape_all.sh
+++ scrape_all.sh
@@ -7,9 +7,11 @@ twitter_data=~/Dropbox/sivb/data/in/twitter
 cd $twitter_data
 
 start_date=2023-02-28
+end_date=2023-03-11
 
 for tag in SVB SBNY FRC
 do
-	snscrape --jsonl --since $start_date twitter-hashtag $tag > hashtag_$tag.json
-	snscrape --jsonl --since $start_date twitter-cashtag $tag > cashtag_$tag.json
+	#snscrape --jsonl --since $start_date twitter-hashtag $tag > hashtag_$tag.json
+	#snscrape --jsonl --since $start_date twitter-cashtag $tag > cashtag_$tag.json
+	snscrape --jsonl twitter-search "(#$tag) since:$start_date until:$end_date" > search_$tag.json
 done
