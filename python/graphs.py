dfe15195e4b3229c5a43a605c43c0c7dea4654f3
Kazimier Smith <kas1112@stern.nyu.edu>
2023-06-12 15:08:38 -0400

    Graph all banks I look at

--- python/graphs.py
+++ python/graphs.py
@@ -27,46 +27,47 @@ fig = root / 'fig'
 
 default_dpi = 300
 
-symbol = 'SBNY'
+for symbol in ['SVB', 'SBNY', 'FRC']:
+    posts_prices_filename = symbol + '_posts_prices.pickle'
+    posts_prices = pd.read_pickle(data_out / posts_prices_filename)
 
-posts_prices_filename = symbol + '_posts_prices.pickle'
-posts_prices = pd.read_pickle(data_out / posts_prices_filename)
+    # Note that trading SVB was halted at 8:30am NY time on March 10th
 
-# Note that trading was halted at 8:30am NY time on March 10th
+    for i in range(1, 11):
+        to_graph = posts_prices[(posts_prices['time'] >= datetime(2023, 3, i)) &
+                                     (posts_prices['time'] < datetime(2023, 3, i + 1))]
 
-for i in range(1, 11):
-    to_graph = posts_prices[(posts_prices['time'] >= datetime(2023, 3, i)) &
-                                 (posts_prices['time'] < datetime(2023, 3, i + 1))]
+        # Plot price and number of posts over time, with price and number of posts on two separate y-axes
+        figure, ax1 = plt.subplots()
 
-    # Plot price and number of posts over time, with price and number of posts on two separate y-axes
-    figure, ax1 = plt.subplots()
+        color = 'tab:red'
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('Price (minute level)', color = color)
+        ax1.plot(to_graph['time'], to_graph['price'], color = color)
+        ax1.tick_params(axis = 'y', labelcolor = color)
 
-    color = 'tab:red'
-    ax1.set_xlabel('Time')
-    ax1.set_ylabel('Price (minute level)', color = color)
-    ax1.plot(to_graph['time'], to_graph['price'], color = color)
-    ax1.tick_params(axis = 'y', labelcolor = color)
+        # X axis tick labels every 2 hours, labeled with hour
+        ax1.xaxis.set_major_locator(mdates.HourLocator(interval = 2))
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
-    # X axis tick labels every 2 hours, labeled with hour
-    ax1.xaxis.set_major_locator(mdates.HourLocator(interval = 2))
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+        # Rotate x axis tick labels
+        for tick in ax1.get_xticklabels():
+            tick.set_rotation(45)
 
-    # Rotate x axis tick labels
-    for tick in ax1.get_xticklabels():
-        tick.set_rotation(45)
+        ax2 = ax1.twinx()
 
-    ax2 = ax1.twinx()
+        color = 'tab:blue'
+        ax2.set_ylabel('Cumulative # posts (minute level)', color = color)
+        ax2.plot(to_graph['time'], to_graph['cum_num_posts'], color = color)
+        ax2.tick_params(axis = 'y', labelcolor = color)
 
-    color = 'tab:blue'
-    ax2.set_ylabel('Cumulative # posts (minute level)', color = color)
-    ax2.plot(to_graph['time'], to_graph['cum_num_posts'], color = color)
-    ax2.tick_params(axis = 'y', labelcolor = color)
+        title = symbol + ' price and Twitter posts on March ' + str(i) + ' 2023'
+        ax2.set_title(title)
 
-    title = symbol + ' price and Twitter posts on March ' + str(i) + ' 2023'
-    ax2.set_title(title)
+        figure.tight_layout()
+        
+        fig_name = symbol + '_price_posts_' + str(i) + '.png'
+        plt.savefig(fig / fig_name, dpi = default_dpi)
 
-    figure.tight_layout()
-    
-    fig_name = symbol + '_price_posts_' + str(i) + '.png'
-    plt.savefig(fig / fig_name, dpi = default_dpi)
-    plt.clf()
+        # Close the figure to free up memory. Note that plt.clf doesn't free up memory, apparently
+        plt.close()
